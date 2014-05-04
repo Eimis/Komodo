7fc1008be237e591986f8db649a4fde072edc1f7
Eimantas <eimantas.stonys@gmail.com>
2014-05-04 14:53:53 +0300

    staticfiles settings

--- komodo/urls.py
+++ komodo/urls.py
@@ -3,6 +3,9 @@ from komodo.views import Main, Search
 from django.views.generic import RedirectView
 from django.conf.urls import handler404, handler500
 from django.contrib import admin
+
+from django.conf import settings
+from django.conf.urls.static import static
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -12,5 +15,5 @@ urlpatterns = patterns('',
 
 
     url(r'^admin/', include(admin.site.urls)),
-)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
