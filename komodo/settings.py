3b09476b4962e9e3915cd22ecf361d62ca626634
Eimantas <eimantas.stonys@gmail.com>
2014-05-05 19:05:48 +0300

    settings.debug

--- komodo/settings.py
+++ komodo/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = '#(9&c+9y9t@y$ytif79^9&^+-d3%hqpzpqmmj*x$oyzg-jn6&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 TEMPLATE_DEBUG = True
 
